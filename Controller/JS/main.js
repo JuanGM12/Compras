4b78630cbe5afbe8098b2293456428011b1c913e
JUAN DIEGO PATI�O OSORIO <juanpatino301662@correo.itm.edu.co>
2022-11-12 18:22:24 -0500

    Terminado el menú principal

--- Controller/JS/main.js
+++ Controller/JS/main.js
@@ -1,5 +1,8 @@
 const btnDepartamentos = document.getElementById('btn-departamentos'),
-    grid = document.getElementById('grid');
+    grid = document.getElementById('grid'),
+    contenedorEnlacesNav = document.querySelector('#menu .contenedor-enlaces-nav'),
+    contenedorSubCategorias = document.querySelector('#grid .contenedor-subcategorias'),
+    btnCerrarMenu = document.getElementById('btn-menu-cerrar'),
     esDispositivoMovil = () => window.innerWidth <= 800;
 
 btnDepartamentos.addEventListener('mouseover', () => {
@@ -16,11 +19,67 @@ grid.addEventListener('mouseleave', () => {
 
 document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
     elemento.addEventListener('mouseenter', (e) => {
-        document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+        if(!esDispositivoMovil()){document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
             categoria.classList.remove('activo');
             if(categoria.dataset.categoria == e.target.dataset.categoria){
                 categoria.classList.add('activo');
             }
         });
+    }        
     })
 });
+
+//Dispositivo móvil
+
+document.querySelector('#btn-menu-barras').addEventListener('click', (e) => {
+    e.preventDefault();
+    if(contenedorEnlacesNav.classList.contains('activo')){
+        contenedorEnlacesNav.classList.remove('activo');
+        document.querySelector('body').style.overflow= 'visible';
+    } else {
+        contenedorEnlacesNav.classList.add('activo');
+        document.querySelector('body').style.overflow= 'hidden';
+    }
+});
+
+btnDepartamentos.addEventListener('click', (e) => {
+    e.preventDefault();
+    grid.classList.add('activo');
+    btnCerrarMenu.classList.add('activo');
+});
+
+//Boton regresar (Movil)
+document.querySelector('#grid .categorias .btn-regresar').addEventListener('click', (e) => {
+    e.preventDefault();
+    grid.classList.remove('activo');
+    btnCerrarMenu.classList.remove('activo');
+});
+
+document.querySelectorAll('#menu .categorias a').forEach((elemento) => {
+    elemento.addEventListener('click', (e) => {
+        if(esDispositivoMovil()){
+            contenedorSubCategorias.classList.add('activo');
+            document.querySelectorAll('#menu .subcategoria').forEach((categoria) => {
+                categoria.classList.remove('activo');
+                if(categoria.dataset.categoria == e.target.dataset.categoria){
+                    categoria.classList.add('activo');
+                }
+            })
+        }
+    });
+});
+
+document.querySelectorAll('#grid .contenedor-subcategorias .btn-regresar').forEach((boton) => {
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+        contenedorSubCategorias.classList.remove('activo');
+    });
+});
+    
+btnCerrarMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelectorAll('#menu .activo').forEach((elemento) => {
+        elemento.classList.remove('activo');
+    })
+    document.querySelector('body').style.overflow = 'visible';
+})
